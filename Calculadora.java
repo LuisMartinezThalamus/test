739a65ec42bd353912e4422e8da9fe254268c2d6
Carlos Faciano <carlos.faciano@thalamuscorp.com>
2022-06-16 15:51:29 -0300

    T2 Resta en rama nueva

--- Calculadora.java
+++ Calculadora.java
@@ -3,12 +3,22 @@ public class Calculadora {
     public  static Integer suma( Integer a, Integer b) {
         return a+b;
     }
+
+    public  static Integer resta( Integer a, Integer b) {
+        return a-b;
+    }
     // Funcion principal 1  
     public static void main(String[] arg) {
         if ("suma".equals(arg[0])){
             Integer a = new Integer(arg[1]);
             Integer b = new Integer(arg[2]);
             System.out.println("La suma es "+suma(a,b));
+        }
+
+        if ("resta".equals(arg[0])){
+            Integer a = new Integer(arg[1]);
+            Integer b = new Integer(arg[2]);
+            System.out.println("La resta es "+resta(a,b));
         }
         
     }
