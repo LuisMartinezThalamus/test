7244ebf19da93a00de0f0730edcb6adb3ae50b33
LuisMartinezThalamus <luis.martinez@thalamuscorp.com>
2022-06-16 13:26:05 -0500

    T1: Inicial

---
+++ Calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora {
+    // Operaciones
+    public  static Integer suma( Integer a, Integer b) {
+        return a+b;
+    }
+    // Funcion principal
+    public static void main(String[] arg) {
+        if ("suma".equals(arg[0])){
+            Integer a = new Integer(arg[1]);
+            Integer b = new Integer(arg[2]);
+            System.out.println("La suma es "+suma(a,b));
+        }
+        
+    }
+    
+}
