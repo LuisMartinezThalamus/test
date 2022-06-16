efd40510e3f02b396466fdeb19a52ae0b514ebce
Javier Chimirri <javierodrigochimirri@gmail.com>
2022-06-16 15:45:53 -0300

    Raiz cuadrada

--- Calculadora.java
+++ Calculadora.java
@@ -3,6 +3,11 @@ public class Calculadora {
     public  static Integer suma( Integer a, Integer b) {
         return a+b;
     }
+
+    public  static Integer raizCuadrada( Integer a) {
+        return a*a;
+    }
+
     // Funcion principal 1  
     public static void main(String[] arg) {
         if ("suma".equals(arg[0])){
