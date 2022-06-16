9be7d71c89ca38fe4b7316a89041a42f570594f2
matiasfittipaldi <30504701+matiasfittipaldi@users.noreply.github.com>
2022-06-16 15:45:31 -0300

    Matias Multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -3,12 +3,18 @@ public class Calculadora {
     public  static Integer suma( Integer a, Integer b) {
         return a+b;
     }
+
+    public  static Integer multiplicacion( Integer a, Integer b) {
+        return a*b;
+    }
+
     // Funcion principal 1  
     public static void main(String[] arg) {
         if ("suma".equals(arg[0])){
             Integer a = new Integer(arg[1]);
             Integer b = new Integer(arg[2]);
             System.out.println("La suma es "+suma(a,b));
+            System.out.println("La multiplicacion es "+,multiplicacion(a,b));
         }
         
     }
