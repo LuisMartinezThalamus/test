73a9f8eea44c9eecb58ee403c5016e43a190a510
Mariano Cortinez <marianocortinez@advenio.com.ar>
2022-06-16 15:55:11 -0300

    Tx Mariano

--- Calculadora.java
+++ Calculadora.java
@@ -7,6 +7,11 @@ public class Calculadora {
     public  static Integer resta( Integer a, Integer b) {
         return a-b;
     }
+
+    public  static Integer incremento( Integer a) {
+        return a++;
+    }
+
     // Funcion principal 1  
     public static void main(String[] arg) {
         if ("suma".equals(arg[0])){
@@ -19,6 +24,11 @@ public class Calculadora {
             Integer a = new Integer(arg[1]);
             Integer b = new Integer(arg[2]);
             System.out.println("La resta es "+resta(a,b));
+        }
+
+        if ("incremento".equals(arg[0])){
+            Integer a = new Integer(arg[1]);
+            System.out.println("El incremento es "+incremento(a));
         }
         
     }
